186dc1ee160cf824c823028bda5310c80b4a2c0e
Haris Subandie Md Suhaimin <haris@vms.my>
2016-08-18 15:01:38 +0800

    get script

---
+++ get.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+wget -O home.xml http://bit.ly/snitv
+wget -O malaysia.xml http://pastebin.com/raw/acjzkMLS
+wget -O singapore.xml http://pastebin.com/raw/cdsBWHgU
+wget -O indonesia.xml http://pastebin.com/raw/KkJPdvzg
+wget -O movie.xml http://pastebin.com/raw/z1Zs1N0b
+wget -O asia.xml http://pastebin.com/raw/5anqcBd4
+wget -O islamic.xml http://pastebin.com/raw/hrUezyw3
+wget -O arab.xml http://pastebin.com/raw/vDicQ3s3
+wget -O news.xml http://pastebin.com/raw/fjLTBbF6
+wget -O documentary.xml http://pastebin.com/raw/hLwqJryG
+wget -O sports.xml http://pastebin.com/raw/dUsWwXmJ
+wget -O news.xml http://pastebin.com/raw/fjLTBbF6
+wget -O melayu.xml http://pastebin.com/raw/mPWKZnYd
+wget -O kids.xml http://pastebin.com/raw/2V1VvYCq
+wget -O radio.xml http://pastebin.com/raw/fzeqBxdy
